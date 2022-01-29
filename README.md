2ecc18a37168ba4e0f541599bff19e8d3d5c2006
Zecival <Zecivalx@gmail.com>
2022-01-29 10:49:33 -0500

    added casino background image, increased size of cards through zoom

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Milestone-Project-1
+
+This is a two-player representstion of the War Card Game in which you fight a computer. In this game, the deck is shuffled randomly, and then split into two halves face down, and then the player and the computer both flip a card up from the top of the deck. The higher ranking card wins, taking the loser’s card and returning both cards to the bottom of the winner’s deck. This process is done until one player loses all of their cards. 
+
+For the ranking of cards, the highest value will win; this is the order of the cards from lowest to highest: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A. In the case that two cards of the same value are flipped, it results in a draw, and borh cards are returned to the bottom each player’s deck.
--- deck.js
+++ deck.js
@@ -1,68 +1,70 @@
-// All of the code for the deck and cards 
-// Suits and Values uppercased to display them as a global variable in representing the card types
-const SUITS = ["♠", "♣", "♥", "♦"] 
-const VALUES = ["A", "2", "3", "4", "5","6", "7", "8", "9", "10", "J", "Q", "K"]
-//  Able to include a full deck of cards or someone's hand of cards 
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+]
+
 export default class Deck {
-    constructor(cards =  freshDeck()) {
-        this.cards = cards
-    }
+  constructor(cards = freshDeck()) {
+    this.cards = cards
+  }
 
-    get numberOfCards() {
-        return this.cards.length
-    }
+  get numberOfCards() {
+    return this.cards.length
+  }
 
-    pop() {
-        return this.cards.shift()
-    }
+  pop() {
+    return this.cards.shift()
+  }
 
-    push(card) {
-        this.cards.push(card)
+  push(card) {
+    this.cards.push(card)
+  }
+
+  shuffle() {
+    for (let i = this.numberOfCards - 1; i > 0; i--) {
+      const newIndex = Math.floor(Math.random() * (i + 1))
+      const oldValue = this.cards[newIndex]
+      this.cards[newIndex] = this.cards[i]
+      this.cards[i] = oldValue
     }
-    
-    // code block below obtains a random index, swaps the current card with a new card; looping through all cards and swapping with another card
-    shuffle() {
-        for (let i = this.numberOfCards - 1; i > 0; i --) {
-            const newIndex = Math.floor(Math.random() * (i + 1))
-            const oldValue = this.cards[newIndex]
-            this.cards[newIndex] = this.cards[i]
-            this.cards[i] = oldValue
-        }
-    }
+  }
 }
 
 class Card {
-    constructor(suit, value) {
-        this.suit = suit
-        this.value = value
-    }
+  constructor(suit, value) {
+    this.suit = suit
+    this.value = value
+  }
 
-    get color() {
-        return this.suit === '♣' || this.suit === '♠' ? 'black' : 'red'
-    }
+  get color() {
+    return this.suit === "♣" || this.suit === "♠" ? "black" : "red"
+  }
 
-    getHTML() {
-        const cardDiv = document.createElement ('div')
-        cardDiv.innerText = this.suit
-        cardDiv.classList.add("card", this.color)
-        cardDiv.dataset.value = $(this.value); $(this.suit)
-        return cardDiv
-    }
+  getHTML() {
+    const cardDiv = document.createElement("div")
+    cardDiv.innerText = this.suit
+    cardDiv.classList.add("card", this.color)
+    cardDiv.dataset.value = `${this.value} ${this.suit}`
+    return cardDiv
+  }
 }
+
 function freshDeck() {
-    return SUITS.flatMap(suit => {
-        return VALUES.map(value => {
-            return new Card(suit, value) 
-        })
+  return SUITS.flatMap(suit => {
+    return VALUES.map(value => {
+      return new Card(suit, value)
     })
-}
-// creates a brand new deck of cards with all 52 cards, one for each suit and value combination
-// goal: loop through all the suits, loop through all the values and then combine them all together inside one array
-// Flatmap function used to condense multiple arrays into one 
-function freshDeck() {
-      return SUITS.flatMap(suit => {
-          return VALUES.map(value => {
-              return new Card(suit, value)
-          })
-      })
+  })
 }
